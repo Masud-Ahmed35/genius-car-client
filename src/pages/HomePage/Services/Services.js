32f64caaf37095fc45602db67089ca0b1946100c
Masud-Ahmed35 <masud.cse.uap@gmail.com>
2022-11-11 21:54:25 +0600

    bonus module

--- src/pages/HomePage/Services/Services.js
+++ src/pages/HomePage/Services/Services.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import { useRef } from 'react';
 import Service from '../Service/Service';
 
 const Services = () => {
     const [services, setServices] = useState([]);
+    const [isAsc, setIsAsc] = useState(true);
+    const searchRef = useRef();
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
-        fetch(`http://localhost:7007/services`)
+        fetch(`http://localhost:7007/services?search=${search}&order=${isAsc ? 'asc' : 'dsc'}`)
             .then(res => res.json())
             .then(data => {
                 setServices(data);
             })
-    }, [])
+    }, [isAsc, search])
+
+    const handleSearch = () => {
+        setSearch(searchRef.current.value);
+    }
 
     return (
         <div>
@@ -18,6 +26,13 @@ const Services = () => {
                 <p className='text-xl font-bold text-orange-600'>Service</p>
                 <h1 className='font-bold text-5xl my-5'>Our Service Area</h1>
                 <p className='text-gray-500 w-1/2 mx-auto mb-12'>The majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            </div>
+            <div className='flex justify-center'>
+                <input ref={searchRef} type="text" placeholder="Type here" className="input input-bordered input-sm mr-3" />
+                <button onClick={handleSearch} className="btn btn-success btn-sm btn-outline normal-case">Search</button>
+            </div>
+            <div className='mb-10 flex justify-end mr-11'>
+                <button onClick={() => setIsAsc(!isAsc)} className="btn btn-outline normal-case">{isAsc ? 'Price: Low > High' : 'Price: High > Low'}</button>
             </div>
             <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
                 {
